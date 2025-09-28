ed8e8be1be163ec89a10b9a280fc44bf1d7ad6df
jainamd05 <jainam.dudhediya123@gmail.com>
2025-09-28 09:33:34 +0530

    Row and column Validity Check

--- logic.py
+++ logic.py
@@ -10,6 +10,20 @@ def find_empty_cell(board):
     return None
 
 # TODO 2 : To have Row Validity Check
+
+def is_valid_row(board, num, row):   
+    # to check if a number is placed in the row
+    if num in board[row, :]:
+        return False
+    return True
+
 # TODO 3 : To have Column Validity Check
+
+def is_valid_col(board, num, col):
+    # to check if a number is placed in the column
+    if num in board[:, col]:
+        return False
+    return True
+
 # TODO 4 : To have Box Validity Check
 # TODO 5 : To have a function that will check wheter the board is valid or not
