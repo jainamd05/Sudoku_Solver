d6d0ec7bdbe3f0cdb7270477d5a3986854a4f2d4
jainamd05 <jainam.dudhediya123@gmail.com>
2025-09-28 09:26:53 +0530

    TODO list

---
+++ logic.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# TODO 1 : we will first find empty spaces
+# TODO 2 : To have Row Validity Check
+# TODO 3 : To have Column Validity Check
+# TODO 4 : To have Box Validity Check
+# TODO 5 : To have a function that will check wheter the board is valid or not
