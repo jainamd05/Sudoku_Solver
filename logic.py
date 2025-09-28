9f09fae846d60211fc6d1505a3b4198f4ca23608
jainamd05 <jainam.dudhediya123@gmail.com>
2025-09-28 09:30:09 +0530

    Finding Empty Spaces

--- logic.py
+++ logic.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 # TODO 1 : we will first find empty spaces
+
+def find_empty_cell(board):
+    for x in range(9):
+        for y in range(9):
+            if board[x , y] == 0:
+                return x, y  # Returning the row and column of the empty cell
+    return None
+
 # TODO 2 : To have Row Validity Check
 # TODO 3 : To have Column Validity Check
 # TODO 4 : To have Box Validity Check
